6992f581b66c32434ccf710729f8ecbe61c4a3e9
monica-vo <53335579+monica-vo@users.noreply.github.com>
2019-09-29 02:01:13 +0100

    Add files via upload

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,16 @@ const Projects = () => {
        <div>
           <h1>Projects</h1>
           <p>Projects page body content</p>
+          <nav>
+           <ul>
+               <li> <a href="https://monica-vo.github.io/keycodefinder/">KeyCode</a></li>
+               <li> <a href="https://monica-vo.github.io/dicegame/">Dice game</a></li>
+               <li> <a href="https://monica-vo.github.io/zengardenproject/">Zen Garden</a></li>
+               <li><a href="#">Cash Machine</a></li>
+               <li><a href="#">Cyber Pet</a></li>
+               
+           </ul>
+       </nav>
        </div>
     );
 }
