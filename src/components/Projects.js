baadaa7ca02a73913e85cafef53faf640b9f192d
monica-vo <53335579+monica-vo@users.noreply.github.com>
2019-09-30 01:33:00 +0100

    Add files via upload
    added _blank and rel to links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,11 +8,11 @@ const Projects = () => {
                 <h1>Projects</h1>
                 <h3>These are my projects so far</h3>
                 <nav className='links'>
-                    <a href="https://monica-vo.github.io/keycodefinder/"><h4>KeyCode</h4></a>
-                    <a href="https://monica-vo.github.io/dicegame/"><h4>Dice game</h4></a>
-                    <a href="https://monica-vo.github.io/zengardenproject/"><h4>Zen Garden</h4></a>
-                    <a href="#"><h4>Cash Machine</h4></a>
-                    <a href="#"><h4>Cyber Pet</h4></a>
+                    <a href="https://monica-vo.github.io/keycodefinder/" target="_blank" rel="noopener noreferrer"><h4>KeyCode</h4></a>
+                    <a href="https://monica-vo.github.io/dicegame/" target="_blank" rel="noopener noreferrer"><h4>Dice game</h4></a>
+                    <a href="https://monica-vo.github.io/zengardenproject/" target="_blank" rel="noopener noreferrer"><h4>Zen Garden</h4></a>
+                    <a href="#" target="_blank" rel="noopener noreferrer"><h4>Cash Machine</h4></a>
+                    <a href="#" target="_blank" rel="noopener noreferrer"><h4>Cyber Pet</h4></a>
                 </nav>
             </div>
         </div>
